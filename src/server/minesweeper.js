30a303c0341945f2cfc941dfca638faaec856bb0
JustinLinhthasack <168403422+JustinLinhthasack@users.noreply.github.com>
2024-05-14 05:03:06 -0400

    Add basic functions to minesweeper, generates game matrix

---
+++ src/server/minesweeper.js
@@ -0,0 +1,116 @@
+// Minesweeper game logic
+
+const TILE_TYPES = {
+    UNSET: -2,
+    BOMB: -1,
+    // Any other byte is a count referencing the amount of bombs around that tile
+}
+
+class Minesweeper {
+    #sizeX = 25; // Default game size
+    #sizeY = 25;
+    #bombPercent = .2;
+    #creator = null;
+    #gameMatrix = [];
+
+    constructor(sizeHeight, sizeWidth, creator, sessionID) {
+        this.#sizeX = sizeHeight;
+        this.#sizeY = sizeWidth;
+        this.#creator = creator;
+    }
+
+    toString() { // for testing purposes, just prints out the game matrix in console.
+        for (let x = 0; x < this.#sizeX; x++) { 
+            let xString = "";
+            for (let y = 0; y < this.#sizeY; y++) {
+                xString += this.#gameMatrix[x][y] + " "
+            }
+            console.log(xString);
+        }
+    }
+
+    generateMatrixFromTile(startX,startY) { 
+
+        for (let x = 0; x < this.#sizeX; x++) { // Initializes the matrix to unsets
+            this.#gameMatrix[x] = [];
+            for (let y = 0; y < this.#sizeY; y++) {
+                this.#gameMatrix[x][y] = TILE_TYPES.UNSET;
+            }
+        }
+
+        for (let i = -1; i < 2; i++) { // Sets initial 3 by 3 to 0s
+            for (let j = -1; j < 2; j++) {
+                if (startX + i >= this.#sizeX ||  startX + i < 0 || startY + j >= this.#sizeY || startY + j < 0) {
+                    continue;
+                }
+                this.#gameMatrix[startX + i][startY + j] = 0;
+            }
+        }
+
+        console.log("INITIAL BOARD")
+        this.toString();
+    
+
+        let amountOfBombs = (this.#sizeX * this.#sizeY) * this.#bombPercent;
+
+        for (let bombCount = 0; bombCount < amountOfBombs; bombCount++) { // Randomizes bomb placement, ignores initial 0s and other bomb tiles
+            let randomX = Math.floor(Math.random() * this.#sizeX);
+            let randomY = Math.floor(Math.random() * this.#sizeY);
+
+            let tileNumber = this.#gameMatrix[randomX][randomY];
+
+            while (tileNumber == TILE_TYPES.BOMB || tileNumber != TILE_TYPES.UNSET) { // Loops till it finds a spot to guarantee number of bombs. Not sure if there is a better algorithm. Will check when finished.
+                randomX = Math.floor(Math.random() * this.#sizeX);
+                randomY = Math.floor(Math.random() * this.#sizeY);
+
+                tileNumber = this.#gameMatrix[randomX][randomY];
+            }
+
+            this.#gameMatrix[randomX][randomY] = TILE_TYPES.BOMB;
+        }
+        console.log("BOMB PLACEMENT")
+        this.toString();
+
+        for (let x = 0; x < this.#sizeX; x++) { // Turns all unsets to 0s and counts up all tiles surrounding bombs
+            for (let y = 0; y < this.#sizeY; y++) {
+                if (this.#gameMatrix[x][y] == TILE_TYPES.UNSET) {
+                    this.#gameMatrix[x][y] = 0;
+                } else if (this.#gameMatrix[x][y] == TILE_TYPES.BOMB) {
+                    let startX = x;
+                    let startY = y;
+
+                    for (let i = -1; i < 2; i++) { // Counts surrounding tiles up by 1
+                        for (let j = -1; j < 2; j++) {
+                            
+                            if (startX + i >= this.#sizeX ||  startX + i < 0 || startY + j >= this.#sizeY || startY + j < 0) {
+                                continue;
+                            }
+                            let tileType = this.#gameMatrix[startX + i][startY + j];
+                            if (tileType == TILE_TYPES.BOMB) {
+                                continue;
+                            }
+
+                            if (tileType == TILE_TYPES.UNSET) {
+                                this.#gameMatrix[startX + i][startY + j] = 0;
+                            }
+                            this.#gameMatrix[startX + i][startY + j] += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        console.log("FULL BOARD")
+        this.toString();
+
+    }
+
+    checkTile(x,y) {
+
+    }
+
+    
+
+}
+
+module.exports = Minesweeper

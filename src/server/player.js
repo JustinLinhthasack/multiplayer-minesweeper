67c28fab6c12a86d287c3427aee08303060f1c49
JustinLinhthasack <168403422+JustinLinhthasack@users.noreply.github.com>
2024-05-15 03:57:19 -0400

    add basic player class, may add more later

---
+++ src/server/player.js
@@ -0,0 +1,8 @@
+class Player {
+    constructor(name, identifier) {
+        this.name = name;
+        this.identifier = identifier;
+
+        // If I decide to add more stats, it would come here.
+    }
+}
